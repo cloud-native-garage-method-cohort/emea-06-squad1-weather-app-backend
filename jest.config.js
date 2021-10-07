9c8f06584abfeb298294cf2c174755036d357fd5
ChimiDEV <Tim.Walter1996@web.de>
2021-10-07 17:10:35 +0200

    feat: prepare testing

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // The root directory that Jest should scan for tests and modules within
+  rootDir: 'test',
+  // setupFiles: ['./_setup.js'],
+};
